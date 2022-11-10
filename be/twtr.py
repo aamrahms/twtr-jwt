f0dc7c60c3bb4c1facfeb2ef2bdc0131f813d5c5
aamrahms <90725468+aamrahms@users.noreply.github.com>
2022-11-09 19:45:28 -0500

    env vars -network and port

--- be/twtr.py
+++ be/twtr.py
@@ -31,9 +31,9 @@ import jwt
 g = dict()
 
 # mongo
-mongo_client = MongoClient('mongodb://localhost:27017/')
+# mongo_client = MongoClient('mongodb://localhost:27017/')
 # mongo_client = MongoClient("mongodb+srv://admin:admin@tweets.8ugzv.mongodb.net/tweets?retryWrites=true&w=majority")
-# mongo_client=MongoClient("mongodb+srv://aamrah:aamrah@cluster0.mnbhu.mongodb.net/?retryWrites=true&w=majority")
+mongo_client=MongoClient("mongodb+srv://aamrah:aamrah@cluster0.mnbhu.mongodb.net/?retryWrites=true&w=majority")
 # mongo_client=MongoClient("mongodb+srv://vasudhab:vasudha@cluster0.ieaeizf.mongodb.net/?retryWrites=true&w=majority")
 class MyMongo(object):
     def __init__(self, db_name):
